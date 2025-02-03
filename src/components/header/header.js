b89587334e8b04fa93a1ddc8d1c74c6b0e3e6c9c
Amit Suthar <amitsuthar101#@gmail.com>
2025-02-03 17:34:46 +0530

    version 1.6

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,4 @@
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Navbar } from 'react-bootstrap';
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import '../header/header.css';
