a1822d7730872996b77a6fdf3b0c2e6e452a8cf4
Amit Suthar <amitsuthar101#@gmail.com>
2025-02-03 16:53:57 +0530

    iamtech-03-02-2025-v1.1

--- src/components/socialMedia/socialMedia.js
+++ src/components/socialMedia/socialMedia.js
@@ -7,10 +7,10 @@ function Socialmedia() {
   return (
     <ul className='socialMedia-box'>
       <li><h6>Follow Me On</h6></li>
-      <li><a href='mailto:amitsuthar101@gmail.com' target='_blank'><FontAwesomeIcon icon={faEnvelope} /></a></li>
-      <li><a href='https://www.facebook.com/amit.suthar.1232' target='_blank'><FontAwesomeIcon icon={faFacebookF} /></a></li>
-      <li><a href='https://www.instagram.com/iamitsuthar/' target='_blank'><FontAwesomeIcon icon={faInstagram} /></a></li>
-      <li><a href='https://www.linkedin.com/in/amit-suthar-497238aa/' target='_blank'><FontAwesomeIcon icon={faLinkedinIn} /></a></li>
+      <li><a href='mailto:amitsuthar101@gmail.com' target='_blank' rel="noopener noreferrer"><FontAwesomeIcon icon={faEnvelope} /></a></li>
+      <li><a href='https://www.facebook.com/amit.suthar.1232' target='_blank' rel="noopener noreferrer"><FontAwesomeIcon icon={faFacebookF} /></a></li>
+      <li><a href='https://www.instagram.com/iamitsuthar/' target='_blank' rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram} /></a></li>
+      <li><a href='https://www.linkedin.com/in/amit-suthar-497238aa/' target='_blank' rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedinIn} /></a></li>
     </ul>
   );
 }
